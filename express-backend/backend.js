d8ac35064e333ce5c6d27853f9e4ffada7701e00
gt <gthapa01@calpoly.edu>
2026-01-21 00:00:04 -0800

    Add DELETE /users/:id hard delete

--- express-backend/backend.js
+++ express-backend/backend.js
@@ -11,6 +11,12 @@ const addUser = (user) => {
   users.users_list.push(user);
   return user;
 };
+const deleteUserById = (id) => {
+  const index = users.users_list.findIndex((user) => user.id === id);
+  if (index === -1) return false;
+  users.users_list.splice(index, 1);
+  return true;
+};
 
 const app = express();
 const port = 8000;
@@ -41,6 +47,16 @@ app.post("/users", (req, res) => {
   const userToAdd = req.body;
   addUser(userToAdd);
   res.status(200).send();
+});
+app.delete("/users/:id", (req, res) => {
+  const id = req.params.id;
+  const deleted = deleteUserById(id);
+
+  if (!deleted) {
+    res.status(404).send("Resource not found.");
+  } else {
+    res.status(204).send(); // No Content is a nice REST choice for successful delete
+  }
 });
 
 const users = {
