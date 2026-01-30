1a1c9626eae8eca3dbf955775e625ea2c95d9ba7
gt <gthapa01@calpoly.edu>
2026-01-30 10:12:09 -0800

    added a comment

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -1,5 +1,16 @@
 // index.js
 import express from "express";
+import cors from "cors";
+
+const users = {
+  users_list: [
+    { id: "xyz789", name: "Charlie", job: "Janitor" },
+    { id: "abc123", name: "Mac", job: "Bouncer" },
+    { id: "ppp222", name: "Mac", job: "Professor" },
+    { id: "yat999", name: "Dee", job: "Aspring actress" },
+    { id: "zap555", name: "Dennis", job: "Bartender" }
+  ]
+}; 
 
 const findUserByName = (name) => {
   return users.users_list.filter((user) => user.name === name);
@@ -25,9 +36,15 @@ const findUsers = ({ name, job }) => {
   });
 };
 
+const generateId = () => {
+  return Date.now().toString(36) + Math.random().toString(36).slice(2, 8);
+};
+
+
 const app = express();
 const port = 8000;
 
+app.use(cors());
 app.use(express.json());
 
 app.get("/users", (req, res) => {
@@ -52,9 +69,18 @@ app.get("/users/:id", (req, res) => {
 });
 app.post("/users", (req, res) => {
   const userToAdd = req.body;
-  addUser(userToAdd);
-  res.status(200).send();
+
+  if (!userToAdd || userToAdd.name === undefined || userToAdd.job === undefined) {
+    return res.status(400).send({ error: "User must include name and job" });
+  }
+
+  const newUser = { ...userToAdd, id: generateId() };
+  addUser(newUser);
+
+  // 201 Created + return created resource
+  return res.status(201).send(newUser);
 });
+
 app.delete("/users/:id", (req, res) => {
   const id = req.params.id;
   const deleted = deleteUserById(id);
@@ -76,16 +102,6 @@ app.get("/users", (req, res) => {
   }
 });
 
-const users = {
-  users_list: [
-    { id: "xyz789", name: "Charlie", job: "Janitor" },
-    { id: "abc123", name: "Mac", job: "Bouncer" },
-    { id: "ppp222", name: "Mac", job: "Professor" },
-    { id: "yat999", name: "Dee", job: "Aspring actress" },
-    { id: "zap555", name: "Dennis", job: "Bartender" }
-  ]
-};
-
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
@@ -97,3 +113,4 @@ app.get("/users", (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
+
